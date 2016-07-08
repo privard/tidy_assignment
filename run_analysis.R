f7b7e2122035db113fee04ad604bf6c26099802c
Philippe Vachon-Rivard <pvachon@touchtunes.com>
2016-07-08 17:01:04 -0400

    Changed writing settings for tidydata.txt

--- run_analysis.R
+++ run_analysis.R
@@ -43,4 +43,4 @@ colnames(final_data) <- tolower(gsub('\\.', '', names(final_data)))
 grouped <- group_by(final_data, activity, subject)
 summarized <- summarise_each(grouped, funs(mean))
 
-write.csv(summarized, file = 'tidydata.csv')
+write.table(summarized, file = 'tidydata.txt', row.name = FALSE)
